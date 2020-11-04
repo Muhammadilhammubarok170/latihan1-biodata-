4c1b8cb96bfa6e665ee9b4e2f1b9108f8ca7c2ac
ilhammubarok170 <ilhammubarok170@gmail.com>
2020-11-04 20:31:27 +0700

    first commit

---
+++ BIODATA.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+nama_lengkap= str(input('Masukan nama lengkap: '))
+nama_panggilan=str(input('Masukan nama panggilan: '))
+npm = int(input('Masukan Nomor Pokok Mahasiswa: '))
+tl = str(input('Masukan tempat lahir: '))
+tgl = int(input('Masukan tanggal lahir: '))
+tlp = int(input('Masukan nomor telepon anda: '))
+alamat = str(input('Masukan alamat: '))
+umur = 20
+
+print('Assalamualaikum')
+print('My Name is ', nama_lengkap, ',but you can call me', nama_panggilan,'.My NPM is', npm,'.\nl was born in', tl,'and i am',umur,'years old.\nl am very glad if you want to invite my house in',alamat,'.So,dont forget to call me before with number',tlp,)
+print('Thank you.')# latihan1-biodata-
